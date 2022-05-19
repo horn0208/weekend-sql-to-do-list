06e1d2d6d7f8bb05bb461305fded341330be352c
Liz Horn <liz.mpls@gmail.com>
2022-05-19 12:47:18 -0500

    sql queries and table creation

---
+++ todo.sql
@@ -0,0 +1,11 @@
+CREATE TABLE tasks (
+	"id" SERIAL PRIMARY KEY,
+	"description" varchar(128),
+	"complete" boolean DEFAULT false
+)
+
+INSERT INTO tasks (description) VALUES ('compliment the dog');
+INSERT INTO tasks (description) VALUES ('give snacks to the dog');
+SELECT * FROM tasks ORDER BY id ASC;
+UPDATE tasks SET complete = true WHERE id=1;
+DELETE FROM tasks WHERE id=1;
