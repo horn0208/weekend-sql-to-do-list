ae7c37debde55ee6c27ca83708bf1d94c2eb70a9
Liz Horn <liz.mpls@gmail.com>
2022-05-24 11:00:33 -0500

    styled order button

--- todo.sql
+++ todo.sql
@@ -9,3 +9,5 @@ INSERT INTO tasks (description) VALUES ('give snacks to the dog');
 SELECT * FROM tasks ORDER BY id ASC;
 UPDATE tasks SET complete = true WHERE id=1;
 DELETE FROM tasks WHERE id=1;
+-- for sort button:
+SELECT * FROM tasks ORDER BY complete DESC;
